78ff983879bafbfaade3b9771b3baaf46bbc7af5
MrMidex <79658753+MrMidex@users.noreply.github.com>
2023-03-05 00:46:30 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,18 +1,20 @@
 <?php
-$login = $_POST['login'];
-$password = $_POST['password'];
+session_start();
 
-$file = fopen('users/users.txt', 'r');
-while (($line = fgets($file)) !== false) {
-    $user = explode(',', trim($line));
-    if ($user[0] === $login && $user[1] === $password) {
-        session_start();
-        $_SESSION['login'] = $login;
-        header('Location: dashboard.php');
-        exit;
-    }
+// Get the username and password values from the POST data
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+// Check if the username and password are correct
+$users = file("users.txt", FILE_IGNORE_NEW_LINES);
+foreach ($users as $user) {
+	$userData = explode(",", $user);
+	if ($username === $userData[0] && $password === $userData[1]) {
+		$_SESSION["username"] = $username;
+		echo "success";
+		exit;
+	}
 }
-fclose($file);
-header('Location: login.php?error=1');
-exit;
+
+echo "failure";
 ?>
